1cbed9fccfe9a2857420129aeb7c4c126774b1a4
Karl Becker <karl@karlbecker.com>
2019-01-12 16:58:51 -0600

    Initial Commit

---
+++ RideshareList/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  RideshareList
+//
+//  Created by Karl Becker on 1/12/19.
+//  Copyright © 2019 KB Productions, LLC. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
---
+++ RideshareListTests/RideshareListTests.swift
@@ -0,0 +1,34 @@
+//
+//  RideshareListTests.swift
+//  RideshareListTests
+//
+//  Created by Karl Becker on 1/12/19.
+//  Copyright © 2019 KB Productions, LLC. All rights reserved.
+//
+
+import XCTest
+@testable import RideshareList
+
+class RideshareListTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
