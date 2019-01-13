3810e5aa10a8bf7c3ad31c6ea1718b77027a9d11
Karl Becker <karl@karlbecker.com>
2019-01-12 18:28:58 -0600

    Add basic API request and JSON parsing

---
+++ RideshareList/RideshareService.swift
@@ -0,0 +1,39 @@
+//
+//  RideshareService.swift
+//  RideshareList
+//
+//  Created by Karl Becker on 1/12/19.
+//  Copyright © 2019 KB Productions, LLC. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+enum AppPlatform : String, Codable {
+    case ios
+    case android
+}
+
+struct App: Codable {
+    let platform: AppPlatform
+    let url: URL
+}
+
+struct ContactInfo:  Codable {
+    let email: String
+    let phone: String
+}
+
+struct ServiceLocation: Codable {
+    let lat: CLLocationDegrees
+    let long: CLLocationDegrees
+    let radiusInMiles: Float    
+}
+
+struct RideshareService: Codable {
+    let name: String
+    let url: URL
+    let locations: [ServiceLocation]
+    let apps: [App]
+    let contact: ContactInfo
+}
